bab80f63373d6b3eda2dcc23a5ed5e0281fcf9fc
Lander Ver Hoef <lander.ver_hoef@colostate.edu>
2026-09-25 16:33:57 +0000

    Added initial non-accessor versions of packing and unpacking routines, and unit tests

--- src/xarray_bitpacker/bitpacker.py
+++ src/xarray_bitpacker/bitpacker.py
@@ -1,4 +1,124 @@
-from typing import Hashable
+from typing import Hashable, Literal
 
 import numpy as np
 import xarray as xr
+
+
+def packbits(
+    array: xr.DataArray,
+    dim: Hashable,
+    bitorder: Literal["big", "little"] = "big",
+    separator: str = " | ",
+) -> xr.DataArray:
+    """
+    Pack boolean array along given dimension using numpy packbits
+
+    args:
+        array (xarray.DataArray): Boolean array containing multiple flags to be packed.
+            The dimension "dim" must index across the flags to be packed, and that
+            dimension must have a coordinate of the same name with string dtype, where
+            the coordinates give the name for each flag. No name should contain the
+            separator as a substring.
+        dim (hashable): Dimension of array over which bit packing should be
+            done.
+        bitorder ("big" or "little"): The order in which to pack bits; see the docs for
+            numpy.packbits for more info.
+        separator (string): String used to separate flag names in the bitpacked
+            metadata. Default is " | ".
+
+    returns:
+        xarray.DataArray: Unsigned 8-bit integer array with the same shape and
+            dimensions as the input array, except for the dimension "dim" which is
+            removed. Each integer has the flags for that location bit-packed using
+            numpy.bitpack. The "long_name" attribute will have "Bit-packed" prepended if
+            present, and will be created as "Bit-packed flags" if not. The bitorder used
+            is saved as the attribute "bitorder", and the attribute "bit_flags" contains
+            the flag names (extracted from the coordinates on dim in array) separated by
+            the separator value.
+    """
+    try:
+        axis = list(array.dims).index(dim)
+    except ValueError as e:
+        e.add_note(f"dim must be a dimension of array.")
+        raise
+    if array.coords[dim].dtype.type is not np.str_:
+        raise ValueError(
+            f"Dimension '{dim}' must have string dtype; got {array.coords[dim].dtype}"
+        )
+    packed_data = np.packbits(array.values, axis=axis, bitorder=bitorder)
+    output_array = xr.DataArray(packed_data, dims=array.dims, attrs=array.attrs)
+    output_array = output_array.squeeze(dim=dim, drop=True)
+    try:
+        output_array.attrs["long_name"] = "Bit-packed " + array.attrs["long_name"]
+    except KeyError:
+        output_array.attrs["long_name"] = "Bit-packed flags"
+    output_array.attrs["bitorder"] = bitorder
+    output_array.attrs["bit_flags"] = separator.join(array.coords[dim].values)
+    output_array.attrs["bit_flag_separator"] = separator
+    output_array.attrs["valid_range"] = [0, 255]
+    return output_array
+
+
+def unpackbits(
+    array: xr.DataArray,
+    dim: Hashable,
+    axis: int = -1,
+) -> xr.DataArray:
+    """
+    Unpack bit-packed DataArray into a boolean array
+
+    args:
+        array (xarray.DataArray): Bit-packed array of unsigned 8-bit integers. Expected
+            to be in the format output by "dataarray_packbits". Should have a "bitorder"
+            attribute containing the bit ordering for the packing, either "big" or
+            "little", a "bit_flags" attribute which contains the flag names separated by
+            the substring found in the "big_flag_separator" attribute, and a "long_name"
+            attribute starting with "Bit-packed ".
+        dim (hashable): Dimension to be added to index between the unpacked flags.
+        axis (int): Position in the list of dimensions where the new dimension should be
+            added. Default is -1 (end of the list).
+
+    returns:
+        xarray.DataArray: Boolean array of the same shape as the input array, but with a
+            new dimension "dim" added at position "axis". For each position along this
+            dimension, the boolean flag represents the unpacked status of the
+            corresponding bit. The dimension "dim" has a coordinate list of flag names
+            extracted from the attribute "bit_flags" in the input array.
+    """
+    # Insert the new dimension at the appropriate location
+    unpacked_dims = list(array.dims)
+    if axis >= 0:
+        unpacked_dims.insert(axis, dim)
+    else:  # Insert doesn't handle negative indices the same way as numpy
+        unpacked_dims.insert(len(array.dims) + 1 + axis, dim)
+
+    # Unpack the coordinates using our known separator
+    unpacked_coords = array.attrs["bit_flags"].split(array.attrs["bit_flag_separator"])
+
+    # Unpack the data, using the length of our coordinates to not excessively 0-pad
+    unsqueezed_data = np.expand_dims(array.values, axis)
+    unpacked_data = np.unpackbits(
+        unsqueezed_data,
+        axis=axis,
+        bitorder=array.attrs["bitorder"],
+        count=len(unpacked_coords),
+    )
+
+    # Package everything into an array
+    unpacked_array = xr.DataArray(
+        unpacked_data,
+        dims=unpacked_dims,
+        coords={dim: unpacked_coords},
+        attrs=array.attrs,
+    )
+
+    # Adjust array metadata
+    # Remove "Bit-packed " from the beginning of the long name
+    unpacked_array.attrs["long_name"] = unpacked_array.attrs["long_name"][11:]
+    # Reset valid range
+    unpacked_array.attrs["valid_range"] = [0, 1]
+    # Remove no longer relevant flags
+    del unpacked_array.attrs["bitorder"]
+    del unpacked_array.attrs["bit_flags"]
+    del unpacked_array.attrs["bit_flag_separator"]
+    return unpacked_array
---
+++ tests/utests/test_bitpacker.py
@@ -0,0 +1,198 @@
+import unittest
+
+import numpy as np
+import xarray as xr
+
+from xarray_bitpacker import bitpacker
+
+
+class Test_packbits(unittest.TestCase):
+    def test_AllFalseArray_AllZeroPackedArray(self):
+        fake_bool_data = np.full((2, 5), False)
+        fake_array = xr.DataArray(
+            fake_bool_data, dims=["flag", "y"], coords={"flag": ["flag1", "flag2"]}
+        )
+        result = bitpacker.packbits(fake_array, dim="flag")
+        mock_result = xr.DataArray(np.zeros((5,), dtype=int), dims=["y"])
+        xr.testing.assert_equal(result, mock_result)
+
+    def test_AllTrueArray_PackedArrayAll192(self):
+        fake_bool_data = np.full((2, 5), True)
+        fake_array = xr.DataArray(
+            fake_bool_data, dims=["flag", "y"], coords={"flag": ["flag1", "flag2"]}
+        )
+        result = bitpacker.packbits(fake_array, dim="flag")
+        mock_result = xr.DataArray(192 * np.ones((5,), dtype=int), dims=["y"])
+        xr.testing.assert_equal(result, mock_result)
+
+    def test_FirstFlagAllTrueSecondAllFalse_All128(self):
+        fake_bool_data = np.stack([np.full((5,), True), np.full((5,), False)], axis=0)
+        fake_array = xr.DataArray(
+            fake_bool_data, dims=["flag", "y"], coords={"flag": ["flag1", "flag2"]}
+        )
+        result = bitpacker.packbits(fake_array, dim="flag")
+        mock_result = xr.DataArray(128 * np.ones((5,), dtype=int), dims=["y"])
+        xr.testing.assert_equal(result, mock_result)
+
+    def test_FirstFlagAllFalseSecondAllTrue_All64(self):
+        fake_bool_data = np.stack([np.full((5,), False), np.full((5,), True)], axis=0)
+        fake_array = xr.DataArray(
+            fake_bool_data, dims=["flag", "y"], coords={"flag": ["flag1", "flag2"]}
+        )
+        result = bitpacker.packbits(fake_array, dim="flag")
+        mock_result = xr.DataArray(64 * np.ones((5,), dtype=int), dims=["y"])
+        xr.testing.assert_equal(result, mock_result)
+
+    def test_FirstFlagAllFalseSecondAllTrueBitorderLittle_All2(self):
+        fake_bool_data = np.stack([np.full((5,), False), np.full((5,), True)], axis=0)
+        fake_array = xr.DataArray(
+            fake_bool_data, dims=["flag", "y"], coords={"flag": ["flag1", "flag2"]}
+        )
+        result = bitpacker.packbits(fake_array, dim="flag", bitorder="little")
+        mock_result = xr.DataArray(2 * np.ones((5,), dtype=int), dims=["y"])
+        xr.testing.assert_equal(result, mock_result)
+
+    def test_DimNotInListOfDims_RaiseException(self):
+        fake_bool_data = np.full((2, 5), False)
+        fake_array = xr.DataArray(
+            fake_bool_data, dims=["flag", "y"], coords={"flag": ["flag1", "flag2"]}
+        )
+        with self.assertRaises(ValueError):
+            _ = bitpacker.packbits(fake_array, "flags")
+
+    def test_DimNot0thInListOfDimsAllFalse_AllZero(self):
+        fake_bool_data = np.full((5, 2), False)
+        fake_array = xr.DataArray(
+            fake_bool_data, dims=["y", "flag"], coords={"flag": ["flag1", "flag2"]}
+        )
+        result = bitpacker.packbits(fake_array, dim="flag")
+        mock_result = xr.DataArray(np.zeros((5,), dtype=int), dims=["y"])
+        xr.testing.assert_equal(result, mock_result)
+
+    def test_NoArrayAttrs_CorrectAttrs(self):
+        fake_bool_data = np.full((5, 2), False)
+        fake_array = xr.DataArray(
+            fake_bool_data, dims=["y", "flag"], coords={"flag": ["flag1", "flag2"]}
+        )
+        result = bitpacker.packbits(fake_array, dim="flag")
+        mock_attrs = {
+            "long_name": "Bit-packed flags",
+            "bitorder": "big",
+            "bit_flags": "flag1 | flag2",
+            "valid_range": [0, 255],
+        }
+        self.assertDictEqual(result.attrs, mock_attrs)
+
+    def test_NoCoordsForDim_RaiseException(self):
+        fake_bool_data = np.full((2, 5), False)
+        fake_array = xr.DataArray(fake_bool_data, dims=["flag", "y"])
+        with self.assertRaises(ValueError):
+            _ = bitpacker.packbits(fake_array, "flag")
+
+
+class Test_unpackbits(unittest.TestCase):
+    def test_AllZeros_UnpackAllFalse(self):
+        fake_packed = xr.DataArray(
+            np.zeros((5,), dtype=np.uint8),
+            dims=["y"],
+            attrs={
+                "long_name": "Bit-packed flags",
+                "bitorder": "little",
+                "bit_flags": "flag_1 | flag_2",
+            },
+        )
+        result = bitpacker.unpackbits(fake_packed, dim="flag")
+        mock_result = xr.DataArray(
+            np.full((5, 2), False),
+            dims=["y", "flag"],
+            coords={"flag": ["flag_1", "flag_2"]},
+        )
+        xr.testing.assert_equal(result, mock_result)
+
+    def test_AllOnesBitorderLittle_UnpackFlag1True(self):
+        fake_packed = xr.DataArray(
+            np.ones((5,), dtype=np.uint8),
+            dims=["y"],
+            attrs={
+                "long_name": "Bit-packed flags",
+                "bitorder": "little",
+                "bit_flags": "flag_1 | flag_2",
+            },
+        )
+        result = bitpacker.unpackbits(fake_packed, dim="flag")
+        mock_result = xr.DataArray(
+            np.stack([np.full((5,), True), np.full((5,), False)], axis=-1),
+            dims=["y", "flag"],
+            coords={"flag": ["flag_1", "flag_2"]},
+        )
+        xr.testing.assert_equal(result, mock_result)
+
+    def test_AllThreesBitorderLittle_UnpackAllFlagsTrue(self):
+        fake_packed = xr.DataArray(
+            3 * np.ones((5,), dtype=np.uint8),
+            dims=["y"],
+            attrs={
+                "long_name": "Bit-packed flags",
+                "bitorder": "little",
+                "bit_flags": "flag_1 | flag_2",
+            },
+        )
+        result = bitpacker.unpackbits(fake_packed, dim="flag")
+        mock_result = xr.DataArray(
+            np.full((5, 2), True),
+            dims=["y", "flag"],
+            coords={"flag": ["flag_1", "flag_2"]},
+        )
+        xr.testing.assert_equal(result, mock_result)
+
+    def test_All128BitorderBig_UnpackFlag1True(self):
+        fake_packed = xr.DataArray(
+            128 * np.ones((5,), dtype=np.uint8),
+            dims=["y"],
+            attrs={
+                "long_name": "Bit-packed flags",
+                "bitorder": "big",
+                "bit_flags": "flag_1 | flag_2",
+            },
+        )
+        result = bitpacker.unpackbits(fake_packed, dim="flag")
+        mock_result = xr.DataArray(
+            np.stack([np.full((5,), True), np.full((5,), False)], axis=-1),
+            dims=["y", "flag"],
+            coords={"flag": ["flag_1", "flag_2"]},
+        )
+        xr.testing.assert_equal(result, mock_result)
+
+    def test_Axis0_UnpackAlongAxis0(self):
+        fake_packed = xr.DataArray(
+            np.zeros((5,), dtype=np.uint8),
+            dims=["y"],
+            attrs={
+                "long_name": "Bit-packed flags",
+                "bitorder": "little",
+                "bit_flags": "flag_1 | flag_2",
+            },
+        )
+        result = bitpacker.unpackbits(fake_packed, dim="flag", axis=0)
+        mock_result = xr.DataArray(
+            np.full((2, 5), False),
+            dims=["flag", "y"],
+            coords={"flag": ["flag_1", "flag_2"]},
+        )
+        xr.testing.assert_equal(result, mock_result)
+
+    def test_BitorderAndBitFlagsNotInAttrs(self):
+        fake_packed = xr.DataArray(
+            np.zeros((5,), dtype=np.uint8),
+            dims=["y"],
+            attrs={
+                "long_name": "Bit-packed flags",
+                "bitorder": "little",
+                "bit_flags": "flag_1 | flag_2",
+            },
+        )
+        result = bitpacker.unpackbits(fake_packed, dim="flag", axis=0)
+        with self.subTest("bitorder"):
+            self.assertNotIn("bitorder", result.attrs)
+        with self.subTest("bit_flags"):
+            self.assertNotIn("bit_flags", result.attrs)
